1f700c38cd9abfadca8d3680010e4b0ce3dce0c2
NaokiHori <36466440+NaokiHori@users.noreply.github.com>
2024-11-24 19:55:08 +0900

    [mod] misc

--- script/main.ts
+++ script/main.ts
@@ -4,56 +4,57 @@ import { getNitems } from "./urlSearchParams";
 import { initSVGObjects, drawSVGObjects } from "./svgObjects";
 import { Counter } from "./counter";
 
+async function main() {
+  const wasm: InitOutput = await initWasm();
+  // number of masses, given by user or decide randomly
+  const nitems: number = getNitems();
+  // fetch DOMs
+  const container: HTMLElement = getHTMLElement("svg-container");
+  const svgElement: SVGElement = getSVGElement("svg-canvas");
+  // create lines and circles inside the given SVG image
+  const { lines, circles }: { lines: Element[]; circles: Element[] } =
+    initSVGObjects(svgElement, nitems);
+  // initialize simulator
+  const wrapper = new Wrapper(nitems);
+  // make the canvas (container / image) full-screen
+  adjustSVGSize(container, svgElement);
+  // register synchronization as an event as well
+  window.addEventListener("resize", () => {
+    adjustSVGSize(container, svgElement);
+  });
+  // compute and print energies periodically
+  const counter = new Counter(100);
+  // animation kernel
+  function updateAndDraw() {
+    // integrate in time to get new information
+    wrapper.integrate();
+    counter.update();
+    // create a view to the shared memory
+    // NOTE: this should be called every time as the pointer seems to be changed
+    const positions = new Float64Array(
+      wasm.memory.buffer,
+      wrapper.get_positions(),
+      nitems * 2,
+    );
+    drawSVGObjects(nitems, positions, lines, circles);
+    if (0 === counter.get()) {
+      const energies: Energy = wrapper.check_energies();
+      const kinetic: number = energies.kinetic;
+      const potential: number = energies.potential;
+      const total: number = kinetic + potential;
+      console.log(
+        `kinetic: ${kinetic.toString()} potential: ${potential.toString()} total: ${total.toString()}`,
+      );
+    }
+    // set myself as the callback
+    requestAnimationFrame(updateAndDraw);
+  }
+  // trigger first animation flow
+  updateAndDraw();
+}
+
 window.addEventListener("load", () => {
-  initWasm()
-    .then((wasm: InitOutput) => {
-      // number of masses, given by user or decide randomly
-      const nitems: number = getNitems();
-      // fetch DOMs
-      const container: HTMLElement = getHTMLElement("svg-container");
-      const svgElement: SVGElement = getSVGElement("svg-canvas");
-      // create lines and circles inside the given SVG image
-      const { lines, circles }: { lines: Element[]; circles: Element[] } =
-        initSVGObjects(svgElement, nitems);
-      // initialize simulator
-      const wrapper = new Wrapper(nitems);
-      // make the canvas (container / image) full-screen
-      adjustSVGSize(container, svgElement);
-      // register synchronization as an event as well
-      window.addEventListener("resize", () => {
-        adjustSVGSize(container, svgElement);
-      });
-      // compute and print energies periodically
-      const counter = new Counter(100);
-      // animation kernel
-      function updateAndDraw() {
-        // integrate in time to get new information
-        wrapper.integrate();
-        counter.update();
-        // create a view to the shared memory
-        // NOTE: this should be called every time as the pointer seems to be changed
-        const positions = new Float64Array(
-          wasm.memory.buffer,
-          wrapper.get_positions(),
-          nitems * 2,
-        );
-        drawSVGObjects(nitems, positions, lines, circles);
-        if (0 === counter.get()) {
-          const energies: Energy = wrapper.check_energies();
-          const kinetic: number = energies.kinetic;
-          const potential: number = energies.potential;
-          const total: number = kinetic + potential;
-          console.log(
-            `kinetic: ${kinetic.toString()} potential: ${potential.toString()} total: ${total.toString()}`,
-          );
-        }
-        // set myself as the callback
-        requestAnimationFrame(updateAndDraw);
-      }
-      // trigger first animation flow
-      updateAndDraw();
-    })
-    .catch(() => {
-      throw new Error("failed to initialize wasm wrapper");
-    });
+  main().catch((error: unknown) => {
+    throw error;
+  });
 });
